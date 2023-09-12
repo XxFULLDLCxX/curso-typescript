8180e71eff3eac07e04cd7eae4d7c4bb158aec46
José Matheus <XxFULLDLCxX@outlook.com>
2023-09-12 19:20:29 -0300

    aula-ts-03

---
+++ aula-ts-03/src/app.ts
@@ -0,0 +1,23 @@
+function multiply(num1: number, num2: number): number {
+  return num1 * num2;
+}
+
+function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function isEven(num: number): boolean {
+  return num % 2 === 0;
+}
+
+function showResult(result: number): void {
+  if (isEven(result)) {
+      console.log(`The result is ${result} and it's even!`);
+  } else {
+      console.log(`The result is ${result} and it's odd!`);
+  }
+}
+
+let result = [multiply(2, 4), sum(1, 2)];
+showResult(result[0]);
+showResult(result[1]);
