b89d15e6b5dd329b0ddd34f2c34da404306159e0
José Matheus <XxFULLDLCxX@outlook.com>
2023-09-13 19:17:28 -0300

    aula-ts-05

---
+++ aula-ts-05/src/app.ts
@@ -0,0 +1,6 @@
+const numbers: Array<Number> = [1,2,3,4,5];
+const anwsers: Array<Boolean> = [true, false, false, true, false];
+const strings: Array<String> = ["one", "two", "three", "four", "five"];
+type T = Number | String;
+const mix: Array<T> = [1,"two",3,"four",5];
+const floatNumbers: Array<Number> = [1.0,2.5,3.0,4.1,5.5];
